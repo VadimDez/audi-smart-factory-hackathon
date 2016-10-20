d650646c3ac09f61d66a891141e2d89ee5293c77
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-10-20 16:24:50 +0200

    changes

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@
 angular.module('audiSmartFactoryHackathonApp', ['audiSmartFactoryHackathonApp.auth',
     'audiSmartFactoryHackathonApp.admin', 'audiSmartFactoryHackathonApp.constants', 'ngCookies',
     'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
-    'validation.match'
+    'validation.match', 'chart.js'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
--- client/app/dashboard/deshboard.controller.js
+++ client/app/dashboard/deshboard.controller.js
@@ -10,14 +10,21 @@
       this.socket = socket;
       this.actual = 0;
       this.max = 16;
+      this.isDetailsShown = false;
+      this.selectedSector = null;
       this.workplaces = [
         [],
         [],
       ];
 
       for (var i = 0; i < this.max; i++) {
-        this.workplaces[0].push({ id: i });
-        this.workplaces[1].push({ id: i });
+        let obj = {
+          id: i,
+          name: 'name...' + i,
+          time: i * Math.random()
+        };
+        this.workplaces[0].push(obj);
+        this.workplaces[1].push(obj);
       }
 
       $interval(() => {
@@ -27,7 +34,47 @@
           this.actual = 0;
         }
       }, 1000);
+
+
+      this.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      this.series = ['Series A', 'Series B'];
+      this.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      this.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      this.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
     }
+
+    showDetails(sector) {
+      this.isDetailsShown = true;
+      this.selectedSector = sector;
+    }
+
+    toggleDetail() {
+      this.isDetailsShown = false;
+    }
+
+    onClick(points, evt) {
+      console.log(points, evt);
+    };
   }
 
   angular.module('audiSmartFactoryHackathonApp')
